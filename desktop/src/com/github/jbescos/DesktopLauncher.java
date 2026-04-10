57684827f68de85e7633d30c153c2390e36d4af0
jbescos <jorge.bescos.gascon@oracle.com>
2026-04-10 06:25:46 +0200

    Full screen
    Signed-off-by: jbescos <jorge.bescos.gascon@oracle.com>

--- desktop/src/com/github/jbescos/DesktopLauncher.java
+++ desktop/src/com/github/jbescos/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.useVsync(true);
-		config.setWindowedMode(1280, 720);
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		config.setResizable(true);
 		config.setTitle("ratass");
 		new Lwjgl3Application(new RatassGame(), config);
